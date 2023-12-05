9504a55681329b0070ec909b870141acbee2f8f9
shreyasjoshi9 <shreyas.joshi9@gmail.com>
2023-12-05 18:10:55 +0530

    Basic Hello World Code in Java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int num = 1;
+        num += 1;
+        float num2 = 2;
+        char alpha = 'a';
+        String name = "shreyas";
+
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(alpha);
+        System.out.println(name);
+        System.out.println("My name is " + name);
+    }
+}
